12f181950ea57aa044f3d440dd6fae82049648d7
justinukas <justinaslolerz@gmail.com>
2024-05-10 15:12:22 +0300

    a little bit of carrot seed action

--- Assets/Scripts/CarrotSeed.cs
+++ Assets/Scripts/CarrotSeed.cs
@@ -4,13 +4,12 @@ using UnityEngine.Events;
 public class CarrotSeedInteraction : MonoBehaviour
 {
     [SerializeField] UnityEvent onTriggerEnter;
-    public GameObject Carrot; // Reference to the wheat prefab
+    public GameObject Carrot; // Reference to the carrot prefab
     private void OnCollisionEnter(Collision other)
     {
         // Check if the seed touches the dirt
         if (other.gameObject.CompareTag("TilledDirt"))
         {
-            onTriggerEnter.Invoke();
             SpawnCarrots(other.gameObject.transform);
             Destroy(gameObject);
         }
@@ -24,14 +23,18 @@ public class CarrotSeedInteraction : MonoBehaviour
         // Define fixed spawn positions relative to the top surface of the dirt
         Vector3[] fixedSpawnOffsets = new Vector3[]
         {
-        new Vector3(-0.45f, 0.5f, -0.45f),
-        new Vector3(0.45f, 0.5f, -0.45f),
-        new Vector3(-0.45f, 0.5f, 0.45f),
-        new Vector3(0.45f, 0.5f, 0.45f),
-        new Vector3(0f, 0.5f, 0f)
+            new Vector3(-0.4f, -0.2f, -0.4f), //bottom right
+            new Vector3(0f, -0.2f, -0.4f), //middle right
+            new Vector3(0f, -0.2f, -0.05f), //middle
+            new Vector3(0f, -0.2f, 0.3f), //middle left
+            new Vector3(0.4f, -0.2f, -0.4f), //top right
+            new Vector3(-0.4f, -0.2f, 0.3f), //bottom left
+            new Vector3(0.4f, -0.2f, 0.3f), //top right
+            new Vector3(0.4f, -0.2f, -0.05f), //middle top
+            new Vector3(-0.4f, -0.2f, -0.05f), //middle bottom
         };
 
-        // Instantiate wheat at fixed locations on the top surface of the dirt
+        // Instantiate carrots at fixed locations on the top surface of the dirt
         foreach (Vector3 offset in fixedSpawnOffsets)
         {
             // Calculate final spawn position with fixed offset
