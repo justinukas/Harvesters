14e1e2b32115c24eb731e797a8bd90ff66834839
justinukas <justinaslolerz@gmail.com>
2024-05-16 20:05:06 +0300

    fog fade in wip

--- Assets/Scripts/wheatseedexperiment.cs
+++ Assets/Scripts/wheatseedexperiment.cs
@@ -26,20 +26,19 @@ public class WheatSeedTest : MonoBehaviour
 
     // make a list
     private List<Vector3> wheatPositionsList = new List<Vector3>();
-
     // add a 5x5 vector3 grid to list
     private void PositionList()
     {
-        float wheatSpawnOffsetX = 0.5f;
+        float wheatSpawnOffsetX = 0.4f;
         float wheatSpawnOffsetZ = 0.45f;
         for (int i = 1; i <= 5; i++)
         {
             for (int j = 1; j <= 4; j++)
             {
-                wheatPositionsList.Add(new Vector3(wheatSpawnOffsetX, 0.5f, wheatSpawnOffsetZ));
+                wheatPositionsList.Add(new Vector3(wheatSpawnOffsetX, 0.3f, wheatSpawnOffsetZ));
                 wheatSpawnOffsetZ -= 0.2f;
             }
-            wheatPositionsList.Add(new Vector3(wheatSpawnOffsetX, 0.5f, wheatSpawnOffsetZ));
+            wheatPositionsList.Add(new Vector3(wheatSpawnOffsetX, 0.3f, wheatSpawnOffsetZ));
             wheatSpawnOffsetX -= 0.2f;
             wheatSpawnOffsetZ = 0.45f;
         }
@@ -50,13 +49,14 @@ public class WheatSeedTest : MonoBehaviour
     {
         // Get the top surface position of the dirt
         Vector3 dirtSurface = dirtPosition + Vector3.up * dirtPosition.y / 2f;
+        Debug.Log(dirtPosition);
         Debug.Log(dirtSurface);
 
         // Instantiate wheat at fixed locations on the top surface of the dirt
         foreach (Vector3 spawnOffset in wheatPositionsList) 
         {
             Vector3 finalSpawnPosition = dirtSurface + spawnOffset; // calculate final spawn position with an offset from list
-            Instantiate(WheatSmall, finalSpawnPosition, Quaternion.Euler(-90f, 0f, 0f)); // instantiate wheat at the calculated position
+            Instantiate(WheatSmall, finalSpawnPosition, Quaternion.Euler(-100, 15, -85)); // instantiate wheat at the calculated position
         }
     }
 }
