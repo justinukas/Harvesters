07a7422b12c7ee2f4aa64e30ced307a99fbdee6b
Bombagladdd <166053818+Bombagladdd@users.noreply.github.com>
2024-11-12 19:04:04 +0200

    Dawg i aint playing the progress is barring and its definitely going
    its going and its definetly progressing i aint playing fr

---
+++ Assets/Harvesters/Plants/Scripts/P.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    private Slider slider;
+
+
+    public float Fillspeed = 0.05f;
+    private float targetProgress = 0;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+
+    //Start is called before the first frame update
+    private void Start()
+    {
+        IncrementProgress(0.75f);
+    }
+
+    //Update is called once per frame
+    private void Update()
+    { 
+        if (slider.value < targetProgress)
+            slider.value += Fillspeed * Time.deltaTime;
+       
+    }
+
+    public void IncrementProgress(float NewProgress)
+    {
+       targetProgress = slider.value + NewProgress;
+    }
+
+
+}
